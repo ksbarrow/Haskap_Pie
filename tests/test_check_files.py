483c3070a6ce5b4a3b806b1666e3eef798b3ebda
Varun Satish <satish5@illinois.edu>
2025-11-15 01:49:28 -0600

    Remove tests

--- tests/test_check_files.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file_list = os.listdir("src/")
-
-if "Find_Refined.py" not in file_list or "haskap.py" not in file_list or "make_pfs_allsnaps.py" not in file_list:
-    print("Required Supporting Modules not present, check installation")
-
-del file_list
